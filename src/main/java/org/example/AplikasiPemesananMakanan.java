ea7a1e9bb78117427466a317dec710e1d1cb3190
Ocellos <selannomicel03@gmail.com>
2023-10-24 00:59:01 +0700

    Implement OOP & Java Collection

--- src/main/java/org/example/AplikasiPemesananMakanan.java
+++ src/main/java/org/example/AplikasiPemesananMakanan.java
@@ -1,12 +1,20 @@
+package org.example;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AplikasiPemesananMakanan {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int pilihan = 0;
+        List<MenuMakanan> daftarMenu = new ArrayList<>();
+        daftarMenu.add(new MenuMakanan("Nasi Goreng", 15000));
+        daftarMenu.add(new MenuMakanan("Mie Ayam", 12000));
+        daftarMenu.add(new MenuMakanan("Soto Ayam", 10000));
 
-        int pesananNasiGoreng = 0; // Variabel untuk menyimpan jumlah pesanan nasi goreng
+        List<Pesanan> pesanan = new ArrayList<>();
 
         do {
             System.out.println("=== Menu Utama ===");
@@ -19,78 +27,63 @@ public class AplikasiPemesananMakanan {
                 pilihan = input.nextInt();
             } catch (InputMismatchException e) {
                 System.out.println("Input tidak sesuai. Masukkan angka (1/2/3).");
-                input.nextLine(); // Membersihkan input yang salah
-                continue; // Melanjutkan ke iterasi berikutnya
+                input.nextLine();
+                continue;
             }
 
             switch (pilihan) {
-                case 1:
-                    // Tambahkan kode untuk menampilkan menu makanan di sini
+                case 1 -> {
                     System.out.println("=== Menu Makanan ===");
-                    System.out.println("1. Nasi Goreng");
-                    System.out.println("2. Mie Ayam");
-                    System.out.println("3. Soto Ayam");
-                    break;
-                case 2:
-                    // Tambahkan kode untuk proses pemesanan makanan di sini
+                    for (int i = 0; i < daftarMenu.size(); i++) {
+                        System.out.println((i + 1) + ". " + daftarMenu.get(i).getNama() + " - " + daftarMenu.get(i).getHarga() + " IDR");
+                    }
+                }
+                case 2 -> {
                     System.out.print("Masukkan nomor menu yang ingin dipesan (1/2/3): ");
-
                     try {
                         int nomorMenu = input.nextInt();
 
-                        switch (nomorMenu) {
-                            case 1:
-                                System.out.print("Masukkan jumlah pesanan Nasi Goreng: ");
-                                int jumlahNasiGoreng = input.nextInt();
-                                pesananNasiGoreng += jumlahNasiGoreng;
-                                System.out.println(jumlahNasiGoreng + " Nasi Goreng telah ditambahkan ke pesanan Anda.");
-                                break;
-                            case 2:
-                                // Tambahkan kode untuk pesanan Mie Ayam di sini
-                                break;
-                            case 3:
-                                // Tambahkan kode untuk pesanan Soto Ayam di sini
-                                break;
-                            default:
-                                System.out.println("Nomor menu tidak valid.");
-                                break;
+                        if (nomorMenu >= 1 && nomorMenu <= daftarMenu.size()) {
+                            System.out.print("Masukkan jumlah pesanan: ");
+                            int jumlahPesanan = input.nextInt();
+                            Pesanan pesan = new Pesanan(daftarMenu.get(nomorMenu - 1), jumlahPesanan);
+                            pesanan.add(pesan);
+                            System.out.println(pesan + " telah ditambahkan ke pesanan Anda.");
+                        } else {
+                            System.out.println("Nomor menu tidak valid.");
                         }
                     } catch (InputMismatchException e) {
                         System.out.println("Input tidak sesuai. Masukkan angka.");
-                        input.nextLine(); // Membersihkan input yang salah
+                        input.nextLine();
                     }
-                    break;
-                case 3:
-                    // Tambahkan layar konfirmasi dan pembayaran di sini
+                }
+                case 3 -> {
                     System.out.println("=== Konfirmasi dan Pembayaran ===");
-                    System.out.println("Pesanan Anda:");
-                    System.out.println("- Nasi Goreng: " + pesananNasiGoreng + " porsi");
-                    // Tambahkan detail pesanan untuk makanan lainnya jika ada
-                    System.out.println("Total Pembayaran: " + calculateTotalPayment(pesananNasiGoreng) + " IDR");
-                    System.out.print("Apakah Anda ingin mengkonfirmasi pesanan dan membayar (ya/tidak)? ");
-                    String konfirmasi = input.next().toLowerCase();
-                    if (konfirmasi.equals("ya")) {
-                        // Tambahkan logika pembayaran di sini
-                        System.out.println("Terima kasih atas pesanan Anda!");
-                        pesananNasiGoreng = 0; // Reset pesanan setelah pembayaran
+                    int totalPembayaran = 0;
+                    if (!pesanan.isEmpty()) {
+                        System.out.println("Pesanan Anda:");
+                        for (Pesanan p : pesanan) {
+                            System.out.println("- " + p.toString());
+                            totalPembayaran += p.hitungTotal();
+                        }
+                        System.out.println("Total Pembayaran: " + totalPembayaran + " IDR");
+                        System.out.print("Apakah Anda ingin mengkonfirmasi pesanan dan membayar (ya/tidak)? ");
+                        String konfirmasi = input.next().toLowerCase();
+                        if (konfirmasi.equals("ya")) {
+                            System.out.println("Terima kasih atas pesanan Anda!");
+                            pesanan.clear();
+                        } else {
+                            System.out.println("Pesanan dibatalkan.");
+                        }
                     } else {
-                        System.out.println("Pesanan dibatalkan.");
+                        System.out.println("Tidak ada pesanan yang dibuat.");
                     }
-                    break;
-                default:
-                    System.out.println("Pilihan tidak valid. Silakan pilih lagi.");
-                    break;
+                }
+                default -> System.out.println("Pilihan tidak valid. Silakan pilih lagi.");
             }
 
         } while (pilihan != 3);
 
         input.close();
-    }
-
-    // Metode untuk menghitung total pembayaran
-    private static int calculateTotalPayment(int pesananNasiGoreng) {
-        // Tambahkan logika perhitungan total pembayaran sesuai harga makanan dan jumlah pesanan
-        int hargaNasiGoreng = 15000; // Harga per porsi Nasi Goreng
-        return pesananNasiGoreng * hargaNasiGoreng;
     }
 }
